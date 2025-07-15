6fd77d315fef9f65dfa179913170daeeb5f78dd7
zhuyilin <zhuyilin9@huawei.com>
2025-07-15 17:51:45 +0800

    support RowParallelLinear
    Signed-off-by: zhuyilin <zhuyilin9@huawei.com>

--- vllm_ascend/models/qwen2.py
+++ vllm_ascend/models/qwen2.py
@@ -6,21 +6,26 @@ from torch import nn
 import torch.nn.functional as F
 from transformers import Qwen2Config
 
-from vllm.attention import AttentionType
+from vllm.attention import Attention, AttentionType
 from vllm.compilation.decorators import support_torch_compile
 from vllm.config import CacheConfig, VllmConfig
 from vllm.distributed import get_pp_group, get_tensor_model_parallel_world_size
-from vllm.model_executor.layers.layernorm import RMSNorm
 from vllm.model_executor.layers.logits_processor import LogitsProcessor
+from vllm.model_executor.layers.activation import SiluAndMul
+from vllm.model_executor.layers.layernorm import RMSNorm
+from vllm.model_executor.layers.linear import (MergedColumnParallelLinear,
+                                               QKVParallelLinear,
+                                               RowParallelLinear)
 from vllm.model_executor.layers.quantization import QuantizationConfig
+from vllm.model_executor.layers.rotary_embedding import get_rope
 from vllm.model_executor.layers.vocab_parallel_embedding import ParallelLMHead
 from vllm.model_executor.sampling_metadata import SamplingMetadata
 from vllm.sequence import IntermediateTensors
 
 from vllm.model_executor.models.interfaces import SupportsLoRA, SupportsPP
-from vllm.model_executor.models.utils import (AutoWeightsLoader, PPMissingLayer, maybe_prefix)
+from vllm.model_executor.models.utils import (AutoWeightsLoader, PPMissingLayer, maybe_prefix, extract_layer_index)
 
-from vllm.model_executor.models.qwen2 import Qwen2Model, Qwen2DecoderLayer, Qwen2Attention, Qwen2MLP
+from vllm.model_executor.models.qwen2 import Qwen2Model, Qwen2DecoderLayer
 from vllm.distributed import (
                             get_pp_group,
                             get_tensor_model_parallel_world_size,
@@ -31,7 +36,7 @@ from vllm.distributed import (
 from vllm_ascend.attention.attention_v1 import AscendAttentionState
 from vllm.forward_context import get_forward_context
 import vllm_ascend.envs as ascend_envs
-
+from vllm_ascend.ops.linear import MaybeScatterRowParallelLinear
 
 def all_gather_and_maybe_unpad(
     hidden_states: torch.Tensor,
@@ -42,17 +47,58 @@ def all_gather_and_maybe_unpad(
         return hidden_states[:-pad_size, :]
     return hidden_states
 
-def maybe_pad_and_reduce_scatter(
-    hidden_states: torch.Tensor,
-    pad_size: int,
-) -> torch.Tensor:
-    if pad_size > 0:
-        hidden_states = F.pad(hidden_states, (0, 0, 0, pad_size))
-    hidden_states = tensor_model_parallel_reduce_scatter(hidden_states, 0)
-    return hidden_states
+class CustomQwen2MLP(nn.Module):
+
+    def __init__(
+        self,
+        hidden_size: int,
+        intermediate_size: int,
+        hidden_act: str,
+        quant_config: Optional[QuantizationConfig] = None,
+        prefix: str = "",
+    ) -> None:
+        super().__init__()
+        self.fc_level = ascend_envs.VLLM_ASCEND_ENABLE_FLASHCOMM 
+        
+        self.gate_up_proj = MergedColumnParallelLinear(
+            hidden_size,
+            [intermediate_size] * 2,
+            bias=False,
+            quant_config=quant_config,
+            prefix=f"{prefix}.gate_up_proj",
+        )
+        if self.fc_level == 1:
+            self.down_proj = MaybeScatterRowParallelLinear(
+                            intermediate_size,
+                            hidden_size,
+                            bias=False,
+                            quant_config=quant_config,
+                            prefix=f"{prefix}.down_proj",
+                            )
+        else:
+            self.down_proj = RowParallelLinear(
+                intermediate_size,
+                hidden_size,
+                bias=False,
+                quant_config=quant_config,
+                prefix=f"{prefix}.down_proj",
+            )
+        if hidden_act != "silu":
+            raise ValueError(f"Unsupported activation: {hidden_act}. "
+                             "Only silu is supported for now.")
+        self.act_fn = SiluAndMul()
+
+    def forward(self, x, with_prefill, pad_size):
+        gate_up, _ = self.gate_up_proj(x)
+        x = self.act_fn(gate_up)
+        if self.fc_level == 1:
+            x, _ = self.down_proj(x, with_prefill, pad_size)
+        else:
+            x, _ = self.down_proj(x)
+        return x
 
 
-class CustomQwen2Attention(Qwen2Attention):
+class CustomQwen2Attention(nn.Module):
 
     def __init__(
         self,
@@ -68,33 +114,96 @@ class CustomQwen2Attention(Qwen2Attention):
         attn_type: str = AttentionType.DECODER,
         dual_chunk_attention_config: Optional[dict[str, Any]] = None,
     ) -> None:
-        super().__init__(hidden_size=hidden_size,
-                         num_heads=num_heads,
-                         num_kv_heads=num_kv_heads,
-                         max_position=max_position,
-                         rope_theta=rope_theta,
-                         cache_config=cache_config,
-                         quant_config=quant_config,
-                         rope_scaling=rope_scaling,
-                         prefix=prefix,
-                         attn_type=attn_type,
-                         dual_chunk_attention_config=dual_chunk_attention_config)
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.tp_size = get_tensor_model_parallel_world_size()
+        self.total_num_heads = num_heads
+        assert self.total_num_heads % self.tp_size == 0
+        self.num_heads = self.total_num_heads // self.tp_size
+        self.total_num_kv_heads = num_kv_heads
+        if self.total_num_kv_heads >= self.tp_size:
+            # Number of KV heads is greater than TP size, so we partition
+            # the KV heads across multiple tensor parallel GPUs.
+            assert self.total_num_kv_heads % self.tp_size == 0
+        else:
+            # Number of KV heads is less than TP size, so we replicate
+            # the KV heads across multiple tensor parallel GPUs.
+            assert self.tp_size % self.total_num_kv_heads == 0
+        self.num_kv_heads = max(1, self.total_num_kv_heads // self.tp_size)
+        self.head_dim = hidden_size // self.total_num_heads
+        self.q_size = self.num_heads * self.head_dim
+        self.kv_size = self.num_kv_heads * self.head_dim
+        self.scaling = self.head_dim**-0.5
+        self.rope_theta = rope_theta
+        self.dual_chunk_attention_config = dual_chunk_attention_config
+        self.fc_level = ascend_envs.VLLM_ASCEND_ENABLE_FLASHCOMM
+
+        self.qkv_proj = QKVParallelLinear(
+            hidden_size,
+            self.head_dim,
+            self.total_num_heads,
+            self.total_num_kv_heads,
+            bias=True,
+            quant_config=quant_config,
+            prefix=f"{prefix}.qkv_proj",
+        )
+        if self.fc_level == 1:
+            self.o_proj = MaybeScatterRowParallelLinear(
+                self.total_num_heads * self.head_dim,
+                hidden_size,
+                bias=False,
+                quant_config=quant_config,
+                prefix=f"{prefix}.o_proj",
+            )
+        else:
+            self.o_proj = RowParallelLinear(
+                self.total_num_heads * self.head_dim,
+                hidden_size,
+                bias=False,
+                quant_config=quant_config,
+                prefix=f"{prefix}.o_proj",
+            )
+
+        self.rotary_emb = get_rope(
+            self.head_dim,
+            rotary_dim=self.head_dim,
+            max_position=max_position,
+            base=self.rope_theta,
+            rope_scaling=rope_scaling,
+            dual_chunk_attention_config=dual_chunk_attention_config,
+        )
+        self.attn = Attention(
+            self.num_heads,
+            self.head_dim,
+            self.scaling,
+            num_kv_heads=self.num_kv_heads,
+            cache_config=cache_config,
+            quant_config=quant_config,
+            attn_type=attn_type,
+            prefix=f"{prefix}.attn",
+            **{
+                "layer_idx": extract_layer_index(prefix),
+                "dual_chunk_attention_config": dual_chunk_attention_config,
+            } if dual_chunk_attention_config else {})
 
     def forward(
         self,
         positions: torch.Tensor,
         hidden_states: torch.Tensor,
-        cos: torch.Tensor,
-        sin: torch.Tensor
-    ) -> torch.Tensor:
+        with_prefill: bool,
+        pad_size: int,
+    ) -> tuple[torch.Tensor, int]:
         qkv, _ = self.qkv_proj(hidden_states)
         q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
-        q, k = self.rotary_emb(positions, q, k, cos=cos, sin=sin, skip_index_select=True)
+        q, k = self.rotary_emb(positions, q, k)
         attn_output = self.attn(q, k, v)
-        output, _ = self.o_proj(attn_output)
+        if self.fc_level == 1:
+            output, _ = self.o_proj(attn_output, with_prefill, pad_size)
+        else:
+            output, _ = self.o_proj(attn_output)
         return output
 
-
+        
 class CustomQwen2DecoderLayer(nn.Module):
 
     def __init__(
@@ -106,13 +215,14 @@ class CustomQwen2DecoderLayer(nn.Module):
     ) -> None:
         super().__init__()
         self.hidden_size = config.hidden_size
+        self.tp_rank = get_tensor_model_parallel_rank()
+        self.tp_size = get_tensor_model_parallel_world_size()
         # Requires transformers > 4.32.0
         rope_theta = getattr(config, "rope_theta", 1000000)
         rope_scaling = getattr(config, "rope_scaling", None)
         dual_chunk_attention_config = getattr(config,
                                               "dual_chunk_attention_config",
                                               None)
-
         # By default, Qwen2 uses causal attention as it is a decoder-only model.
         # You can override the HF config with `is_causal=False` to enable
         # bidirectional attention, which is used in some embedding models
@@ -135,7 +245,7 @@ class CustomQwen2DecoderLayer(nn.Module):
             attn_type=attn_type,
             dual_chunk_attention_config=dual_chunk_attention_config,
         )
-        self.mlp = Qwen2MLP(
+        self.mlp = CustomQwen2MLP(
             hidden_size=self.hidden_size,
             intermediate_size=config.intermediate_size,
             hidden_act=config.hidden_act,
@@ -147,18 +257,11 @@ class CustomQwen2DecoderLayer(nn.Module):
         self.post_attention_layernorm = RMSNorm(config.hidden_size,
                                                 eps=config.rms_norm_eps)
         
-        self.tp_rank = get_tensor_model_parallel_rank()
-        self.tp_size = get_tensor_model_parallel_world_size()
-        self.self_attn.o_proj.reduce_results=False
-        self.mlp.down_proj.reduce_results=False
-        
     def forward(
         self,
         positions: torch.Tensor,
         hidden_states: torch.Tensor,
         residual: Optional[torch.Tensor],
-        cos,
-        sin,
         flashcomm_v1_enabled: bool,
         pad_size: int,
     ) -> tuple[torch.Tensor, torch.Tensor]:
@@ -178,23 +281,15 @@ class CustomQwen2DecoderLayer(nn.Module):
         hidden_states = self.self_attn(
             positions=positions,
             hidden_states=hidden_states,
-            cos=cos,
-            sin=sin
+            with_prefill=flashcomm_v1_enabled,
+            pad_size=pad_size,
         )
-        if flashcomm_v1_enabled:
-            hidden_states = maybe_pad_and_reduce_scatter(hidden_states, pad_size)
-        else:
-            hidden_states = tensor_model_parallel_all_reduce(hidden_states)
         # Fully Connected
         hidden_states, residual = self.post_attention_layernorm(
             hidden_states, residual)
         if flashcomm_v1_enabled:
             hidden_states = all_gather_and_maybe_unpad(hidden_states, pad_size)
-        hidden_states = self.mlp(hidden_states)
-        if flashcomm_v1_enabled:
-            hidden_states = maybe_pad_and_reduce_scatter(hidden_states, pad_size)
-        else:
-            hidden_states = tensor_model_parallel_all_reduce(hidden_states)
+        hidden_states = self.mlp(hidden_states, flashcomm_v1_enabled, pad_size)
         return hidden_states, residual
 
 
@@ -218,7 +313,6 @@ class CustomQwen2Model(Qwen2Model):
                          prefix=prefix,
                          decoder_layer_type=decoder_layer_type)
         self.tp_size = get_tensor_model_parallel_world_size()
-        self.cos_sin_cache = self.layers[0].self_attn.rotary_emb.cos_sin_cache
 
     def forward(
         self,
@@ -247,17 +341,11 @@ class CustomQwen2Model(Qwen2Model):
         if flashcomm_v1_enabled:
             num_tokens = hidden_states.size(0)
             pad_size = (self.tp_size - (num_tokens % self.tp_size)) % self.tp_size
-        cos_sin = self.cos_sin_cache.index_select(0, positions)
-        last_dim = cos_sin.size()[-1]
-        cos, sin = cos_sin.reshape(-1 ,2, last_dim // 2).repeat(1, 1, 2).chunk(2, dim=-2)
-        cos, sin = cos.view(1, -1, 1, last_dim).contiguous(), sin.view(1, -1, 1, last_dim).contiguous()
         for layer in self.layers[self.start_layer:self.end_layer]:
             hidden_states, residual = layer(
                 positions,
                 hidden_states,
                 residual,
-                cos,
-                sin,
                 flashcomm_v1_enabled,
                 pad_size,
             )
---
+++ vllm_ascend/ops/linear.py
@@ -0,0 +1,141 @@
+import itertools
+from abc import abstractmethod
+from typing import Any, Literal, Optional, Union
+
+import torch
+import torch.nn as nn
+from torch.nn.parameter import Parameter, UninitializedParameter
+import torch_npu
+from vllm.distributed import (divide, get_tensor_model_parallel_rank,
+                              get_tensor_model_parallel_world_size,
+                              split_tensor_along_last_dim,
+                              tensor_model_parallel_all_gather,
+                              tensor_model_parallel_all_reduce,
+                              tensor_model_parallel_reduce_scatter)
+from vllm.logger import init_logger
+from vllm.model_executor.layers.quantization.base_config import (
+    QuantizationConfig, QuantizeMethodBase)
+from vllm.model_executor.layers.utils import dispatch_unquantized_gemm
+# yapf: disable
+from vllm.model_executor.parameter import (BasevLLMParameter,
+                                           BlockQuantScaleParameter,
+                                           PackedColumnParameter,
+                                           PackedvLLMParameter,
+                                           PerTensorScaleParameter,
+                                           RowvLLMParameter)
+# yapf: enable
+from vllm.model_executor.utils import set_weight_attrs
+
+from vllm.model_executor.layers.linear import RowParallelLinear
+import torch.nn.functional as F
+from vllm.forward_context import get_forward_context
+from vllm.model_executor.layers.linear import LinearMethodBase
+from vllm.model_executor.layers.utils import dispatch_unquantized_gemm
+logger = init_logger(__name__)
+
+
+class MaybeScatterRowParallelLinear(RowParallelLinear):
+    # Replace all_reduce with reduce_scatter in flashcomm_v1 situation
+    def __init__(self,
+        input_size: int,
+        output_size: int,
+        bias: bool = True,
+        input_is_parallel: bool = True,
+        skip_bias_add: bool = False,
+        params_dtype: Optional[torch.dtype] = None,
+        reduce_results: bool = True,
+        quant_config: Optional[QuantizationConfig] = None,
+        prefix: str = "",
+        *,
+        return_bias: bool = True,):
+        super().__init__(input_size,
+                    output_size,
+                    bias,
+                    input_is_parallel,
+                    skip_bias_add,
+                    params_dtype,
+                    reduce_results,
+                    quant_config,
+                    prefix)
+        self.quant_method = CostomUnquantizedLinearMethod()
+
+    def forward(
+        self, input_, use_scatter, pad_size,
+    ) -> Union[torch.Tensor, tuple[torch.Tensor, Optional[Parameter]]]:
+        if self.input_is_parallel:
+            input_parallel = input_
+        else:
+            tp_rank = get_tensor_model_parallel_rank()
+            splitted_input = split_tensor_along_last_dim(
+                input_, num_partitions=self.tp_size)
+            input_parallel = splitted_input[tp_rank].contiguous()
+
+        # Matrix multiply.
+        assert self.quant_method is not None
+        # Only fuse bias add into GEMM for rank 0 (this ensures that
+        # bias will not get added more than once in TP>1 case)
+        bias_ = None if (self.tp_rank > 0 or self.skip_bias_add) else self.bias
+        # print(f'zyl_before = {input_parallel.shape}')
+        if self.reduce_results and self.tp_size > 1:
+            if use_scatter:
+                output_parallel = self.quant_method.apply_prefill(self,
+                                            input_parallel,
+                                            pad_size,
+                                            bias=bias_)
+                output = output_parallel                       
+            else:
+                output_parallel = self.quant_method.apply(self,
+                                            input_parallel,
+                                            bias=bias_)
+                output = tensor_model_parallel_all_reduce(output_parallel)
+        else:
+            output = output_parallel
+        
+
+        output_bias = self.bias if self.skip_bias_add else None
+
+        if not self.return_bias:
+            return output
+        return output, output_bias
+
+
+class CostomUnquantizedLinearMethod(LinearMethodBase):
+    """Linear method without quantization."""
+
+    def create_weights(self, layer: torch.nn.Module,
+                       input_size_per_partition: int,
+                       output_partition_sizes: list[int], input_size: int,
+                       output_size: int, params_dtype: torch.dtype,
+                       **extra_weight_attrs):
+        weight = Parameter(torch.empty(sum(output_partition_sizes),
+                                       input_size_per_partition,
+                                       dtype=params_dtype),
+                           requires_grad=False)
+        set_weight_attrs(weight, {"input_dim": 1, "output_dim": 0})
+        layer.register_parameter("weight", weight)
+        set_weight_attrs(weight, extra_weight_attrs)
+
+    def apply_prefill(self,
+              layer: torch.nn.Module,   # [5120, 1280]
+              x: torch.Tensor,          # [8192,1280]
+              pad_size: int,
+              bias: Optional[torch.Tensor] = None) -> torch.Tensor:
+        tp_rank = get_tensor_model_parallel_rank()
+        tp_size = get_tensor_model_parallel_world_size()
+        origin_device = x.device
+        origin_dtype = x.dtype
+        if pad_size > 0:
+            x = F.pad(x, (0, 0, 0, pad_size))
+        current_rank = torch.npu.current_device()
+        commDomain = str(current_rank // tp_size)
+        output = torch.empty(x.shape[0] // tp_size, layer.weight.shape[0], dtype=origin_dtype, device=origin_device)
+        torch_npu.atb._npu_matmul_reduce_scatter(x, layer.weight, output, bias, rank=tp_rank, rankSize=tp_size, commDomain=commDomain)
+        # print(f'zyl_module = {output.shape}')
+        return output
+    
+    def apply(self,
+              layer: torch.nn.Module,
+              x: torch.Tensor,
+              bias: Optional[torch.Tensor] = None) -> torch.Tensor:
+        return dispatch_unquantized_gemm()(x, layer.weight, bias)
+    
